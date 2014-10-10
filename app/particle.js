bf8487f286f35fec25245a1add99eead211ad947
Samson Ootoovak <sam@enspiral.com>
2014-10-10 15:13:03 +1300

    Add a Particle object.

---
+++ app/particle.js
@@ -0,0 +1,14 @@
+var Particle = function(position, velocity, acceleration) {
+  this.position     = position     || new Vector(0, 0)
+  this.velocity     = velocity     || new Vector(0, 0)
+  this.acceleration = acceleration || new Vector(0, 0)
+}
+
+Particle.prototype = {
+  move = function () {
+    this.velocity.add(this.acceleration)
+    this.position.add(this.velocity)
+  },
+}
+
+module.exports = Particle
