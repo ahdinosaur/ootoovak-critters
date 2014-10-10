020e31677ffee05a3ad5180b49d18d77307f7d0f
Samson Ootoovak <sam@enspiral.com>
2014-10-10 15:23:31 +1300

    Add Emitter object.

---
+++ app/emitter.js
@@ -0,0 +1,28 @@
+var Vector   = require('./vector')
+var Particle = require('./particle')
+
+var Emitter = function(position, velocity, spread) {
+  this.position  = position
+  this.velocity  = velocity
+  this.spread    = spread || (Math.PI / 16)
+  this.drawColor = '#999'
+}
+
+Emitter.prototype = {
+  emitParticle: function() {
+    var angle     = this.randomAngle()
+    var magnitude = this.velocity.getMagnitude()
+    var position  = new Vector(this.position.x, this.position.y)
+
+    var velocity = Vector.fromAngle(angle, magnitude)
+
+    return new Particle(position, velocity)
+  },
+  randomAngle: function() {
+    var velocityAngle       = this.velocity.getAngle()
+    var randomAngleInSpread = this.spread - (Math.random() * this.spread * 2)
+    return velocityAngle + randomAngleInSpread
+  },
+}
+
+module.exports = Emitter
